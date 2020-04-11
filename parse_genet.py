854fe4f80095ed805bc6e11b47786f42c8616932
Tian Ge <tge1@mgh.harvard.edu>
2020-04-11 17:00:03 -0400

    check strand flip

--- parse_genet.py
+++ parse_genet.py
@@ -62,11 +62,19 @@ def parse_sumstats(ref_dict, vld_dict, sst_file, n_subj):
     print('... %d SNPs read from %s ...' % (len(sst_dict['SNP']), sst_file))
 
 
-    ref_snp = set(zip(ref_dict['SNP'], ref_dict['A1'], ref_dict['A2']))
-    vld_snp = set(zip(vld_dict['SNP'], vld_dict['A1'], vld_dict['A2'])) | set(zip(vld_dict['SNP'], vld_dict['A2'], vld_dict['A1']))
-    sst_snp = set(zip(sst_dict['SNP'], sst_dict['A1'], sst_dict['A2'])) | set(zip(sst_dict['SNP'], sst_dict['A2'], sst_dict['A1']))
+    mapping = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
 
-    comm_snp = ref_snp & vld_snp & sst_snp
+    vld_snp = set(zip(vld_dict['SNP'], vld_dict['A1'], vld_dict['A2']))
+
+    ref_snp = set(zip(ref_dict['SNP'], ref_dict['A1'], ref_dict['A2'])) | set(zip(ref_dict['SNP'], ref_dict['A2'], ref_dict['A1'])) | \
+              set(zip(ref_dict['SNP'], [mapping[aa] for aa in ref_dict['A1']], [mapping[aa] for aa in ref_dict['A2']])) | \
+              set(zip(ref_dict['SNP'], [mapping[aa] for aa in ref_dict['A2']], [mapping[aa] for aa in ref_dict['A1']]))
+
+    sst_snp = set(zip(sst_dict['SNP'], sst_dict['A1'], sst_dict['A2'])) | set(zip(sst_dict['SNP'], sst_dict['A2'], sst_dict['A1'])) | \
+              set(zip(sst_dict['SNP'], [mapping[aa] for aa in sst_dict['A1']], [mapping[aa] for aa in sst_dict['A2']])) | \
+              set(zip(sst_dict['SNP'], [mapping[aa] for aa in sst_dict['A2']], [mapping[aa] for aa in sst_dict['A1']]))
+
+    comm_snp = vld_snp & ref_snp & sst_snp
 
     print('... %d common SNPs in the reference, sumstats, and validation set ...' % len(comm_snp))
 
@@ -79,7 +87,7 @@ def parse_sumstats(ref_dict, vld_dict, sst_file, n_subj):
         for line in ff:
             ll = (line.strip()).split()
             snp = ll[0]; a1 = ll[1]; a2 = ll[2]
-            if (snp, a1, a2) in comm_snp:
+            if (snp, a1, a2) in comm_snp or (snp, mapping[a1], mapping[a2]) in comm_snp:
                 if 'BETA' in header:
                     beta = float(ll[3])
                 elif 'OR' in header:
@@ -88,7 +96,7 @@ def parse_sumstats(ref_dict, vld_dict, sst_file, n_subj):
                 p = max(float(ll[4]), 1e-323)
                 beta_std = sp.sign(beta)*abs(norm.ppf(p/2.0))/n_sqrt
                 sst_eff.update({snp: beta_std})
-            elif (snp, a2, a1) in comm_snp:
+            elif (snp, a2, a1) in comm_snp or (snp, mapping[a2], mapping[a1]) in comm_snp:
                 if 'BETA' in header:
                     beta = float(ll[3])
                 elif 'OR' in header:
@@ -99,16 +107,35 @@ def parse_sumstats(ref_dict, vld_dict, sst_file, n_subj):
                 sst_eff.update({snp: beta_std})
 
 
-    sst_dict = {'CHR':[], 'SNP':[], 'BP':[], 'A1':[], 'A2':[], 'MAF':[], 'BETA':[]}
+    sst_dict = {'CHR':[], 'SNP':[], 'BP':[], 'A1':[], 'A2':[], 'MAF':[], 'BETA':[], 'FLP':[]}
     for (ii, snp) in enumerate(ref_dict['SNP']):
         if snp in sst_eff:
             sst_dict['SNP'].append(snp)
             sst_dict['CHR'].append(ref_dict['CHR'][ii])
             sst_dict['BP'].append(ref_dict['BP'][ii])
-            sst_dict['A1'].append(ref_dict['A1'][ii])
-            sst_dict['A2'].append(ref_dict['A2'][ii])
-            sst_dict['MAF'].append(ref_dict['MAF'][ii])
             sst_dict['BETA'].append(sst_eff[snp])
+
+            a1 = ref_dict['A1'][ii]; a2 = ref_dict['A2'][ii]
+            if (snp, a1, a2) in comm_snp:
+                sst_dict['A1'].append(a1)
+                sst_dict['A2'].append(a2)
+                sst_dict['MAF'].append(ref_dict['MAF'][ii])
+                sst_dict['FLP'].append(1)
+            elif (snp, a2, a1) in comm_snp:
+                sst_dict['A1'].append(a2)
+                sst_dict['A2'].append(a1)
+                sst_dict['MAF'].append(1-ref_dict['MAF'][ii])
+                sst_dict['FLP'].append(-1)
+            elif (snp, mapping[a1], mapping[a2]) in comm_snp:
+                sst_dict['A1'].append(mapping[a1])
+                sst_dict['A2'].append(mapping[a2])
+                sst_dict['MAF'].append(ref_dict['MAF'][ii])
+                sst_dict['FLP'].append(1)
+            elif (snp, mapping[a2], mapping[a1]) in comm_snp:
+                sst_dict['A1'].append(mapping[a2])
+                sst_dict['A2'].append(mapping[a1])
+                sst_dict['MAF'].append(1-ref_dict['MAF'][ii])
+                sst_dict['FLP'].append(-1)
 
     return sst_dict
 
@@ -125,14 +152,16 @@ def parse_ldblk(ldblk_dir, sst_dict, chrom):
     for blk in range(1,n_blk+1):
         snp_blk.append([bb.decode("UTF-8") for bb in list(hdf_chr['blk_'+str(blk)]['snplist'])])
 
-    snp_sst = set(sst_dict['SNP'])
-
     blk_size = []
+    mm = 0
     for blk in range(n_blk):
-        idx = [ii for (ii, snp) in enumerate(snp_blk[blk]) if snp in snp_sst]
+        idx = [ii for (ii, snp) in enumerate(snp_blk[blk]) if snp in sst_dict['SNP']]
         blk_size.append(len(idx))
         if idx != []:
-            ld_blk[blk] = ld_blk[blk][sp.ix_(idx,idx)]
+            idx_blk = range(mm,mm+len(idx))
+            flip = [sst_dict['FLP'][jj] for jj in idx_blk]
+            ld_blk[blk] = ld_blk[blk][sp.ix_(idx,idx)]*sp.outer(flip,flip)
+            mm += len(idx)
         else:
             ld_blk[blk] = sp.array([])
 
